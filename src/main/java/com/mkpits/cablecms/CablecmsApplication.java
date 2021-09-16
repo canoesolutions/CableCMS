501aadf2b16c02d2c27a424301386373dde49c3c
shanpeshwe <shantanupeshwe@gmail.com>
2021-09-16 13:56:26 +0530

    Update

---
+++ src/main/java/com/mkpits/cablecms/CablecmsApplication.java
@@ -0,0 +1,13 @@
+package com.mkpits.cablecms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CablecmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CablecmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkpits/cablecms/CablecmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkpits.cablecms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CablecmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
