ff09712e705a5e9f6967ec96d2f664c57ebc2325
shanpeshwe <shantanupeshwe@gmail.com>
2021-09-17 17:48:51 +0530

    Update

--- src/main/java/com/mkpits/cablecms/controller/CablecmsController.java
+++ src/main/java/com/mkpits/cablecms/controller/CablecmsController.java
@@ -5,20 +5,20 @@ import com.mkpits.cablecms.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/login")
 public class CablecmsController {
     @Autowired
     LoginService loginService;
 
-    @RequestMapping(name="/",method = RequestMethod.GET)
+    @GetMapping("/")
     public String login(Model model) {
 
         LoginResponse loginResponse=loginService.getLogin();
         model.addAttribute("login",loginResponse);
-        return "login";
+        return "index";
     }
 }
