b8b72d41e7be730654adb230dee38878964f2bbc
Harshal Danao <hdanao86@gmail.com>
2021-09-23 00:35:33 -0700

    update

---
+++ src/main/java/com/mkpits/cablecms/dao/ReceiptRepo.java
@@ -0,0 +1,9 @@
+package com.mkpits.cablecms.dao;
+import com.mkpits.cablecms.model.Admin;
+import com.mkpits.cablecms.dto.AdminDto;
+import com.mkpits.cablecms.model.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceiptRepo extends JpaRepository <Receipt,Integer>{
+
+}
---
+++ src/main/java/com/mkpits/cablecms/service/ReceiptService.java
@@ -0,0 +1,20 @@
+package com.mkpits.cablecms.service;
+import com.mkpits.cablecms.dao.AdminRepo;
+import com.mkpits.cablecms.dao.ReceiptRepo;
+import com.mkpits.cablecms.model.Admin;
+import com.mkpits.cablecms.model.Receipt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceiptService {
+    @Autowired
+    ReceiptRepo receiptRepo;
+
+    public Admin createReceipt(Receipt receipt){
+        receiptRepo.save(receipt);
+        //adminRepo.findOne();
+        return null;
+    }
+
+}
